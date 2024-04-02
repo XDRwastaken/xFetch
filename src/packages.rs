f2c142e77ba3a218a22b583637d498c14befb3dc
XDR <xxandrr@proton.me>
2024-04-01 23:56:00 -0500

    Debloat pkgs.rs [CI SKIP]
    Just realized that it's the same as exFetch's

--- src/packages.rs
+++ src/packages.rs
@@ -3,8 +3,5 @@ use std::{fs::read_dir, path::Path};
 const PACMAN_DIR: &str = "/var/lib/pacman/local";
 
 pub fn get_num_packages() -> i16 {
-    match read_dir(Path::new(PACMAN_DIR)) {
-        Ok(entries) => entries.count() as i16 - 1, // -1 because there is an ALPM_DB_VERSION file at that dir
-        Err(_) => return 0
-    }
+    read_dir(Path::new(PACMAN_DIR)).map_or(0, |entries| entries.count() as i16 - 1)
 }
